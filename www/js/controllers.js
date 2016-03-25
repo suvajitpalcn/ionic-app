ad0023af259869e0ddde7469e77a23f279f12dfd
suvajitpalcn <suvajitp.cn@gmail.com>
2016-03-25 15:42:02 +0530

    Adding the first ionic app work.

---
+++ www/js/controllers.js
@@ -0,0 +1,49 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+  $scope.remove = function(friend) {
+    Friends.remove(friend);
+  };
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+
+.controller('TodoCtrl', function($scope, Todos) {
+  $scope.todos = Todos.all();
+  $scope.label = Todos.getCheckText();
+  $scope.pushNotification = { checked: false };
+  $scope.pushNotificationChange = function() {
+    return $scope.pushNotification.checked;
+  };
+});
---
+++ www/js/services.js
@@ -0,0 +1,138 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    location: 'Norway',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    location: 'Texas',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    location: 'San Francisco',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    location: 'Ohio',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    location: 'Las Vegas',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    remove: function(friend) {
+      friends.splice(friends.indexOf(chat), 1);
+    },
+    get: function(friendId) {
+      for (var i = 0; i < friends.length; i++) {
+        if (friends[i].id === parseInt(friendId)) {
+          return friends[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.factory('Todos', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var todos = [{
+    id: 0,
+    text: 'Ben Sparrow',
+    checked: true
+  }, {
+    id: 1,
+    text: 'Max Lynx',
+    checked: false
+  }, {
+    id: 2,
+    text: 'Adam Bradleyson',
+    checked: true
+  }, {
+    id: 3,
+    text: 'Perry Governor',
+    checked: false
+  }, {
+    id: 4,
+    text: 'Mike Harrington',
+    checked: false
+  }];
+
+  return {
+    all: function() {
+      return todos;
+    },
+    remove: function(todo) {
+      todos.splice(todos.indexOf(todo), 1);
+    },
+    getCheckText: function() {
+      return 'Hide Completed?';
+    }
+  };
+});
